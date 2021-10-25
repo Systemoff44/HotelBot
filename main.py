c0d096dcda7dd20ac8979751c45bcf1ace8a0828
Sergey <markstoik44@gmail.com>
2021-10-25 16:49:15 +0300

    created telegrammbot

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import telebot
 
-bot = telebot.TeleBot("YOUR_BOT_TOKEN")
+bot = telebot.TeleBot("2004736420:AAHL4hHRiOySvTP5hTzUp-cucFB0l7jMH80")
 
 
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(content_types=['text'])
 def send_welcome(message):
-	bot.reply_to(message, "Howdy, how are you doing?")
+	if message.text == "Привет":
+		bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+	elif message.text == "/hello-world":
+		bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+	else:
+		bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /hello-world.")
 
 
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-	bot.reply_to(message, message.text)
-
-
-bot.infinity_polling()
+bot.polling(none_stop=True, interval=0)
