649228babefd38b17954d917fe27e844fb743e8a
Sergey <markstoik44@gmail.com>
2021-10-28 16:31:20 +0300

    created file requirements

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import telebot
+from decouple import config
 
-bot = telebot.TeleBot("2004736420:AAHL4hHRiOySvTP5hTzUp-cucFB0l7jMH80")
+
+bot = telebot.TeleBot(config('Token'))
 
 
 @bot.message_handler(content_types=['text'])
 def send_welcome(message):
-	if message.text == "Привет":
-		bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
-	elif message.text == "/hello-world":
-		bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
-	else:
-		bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /hello-world.")
+    if message.text == "Привет":
+        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+    elif message.text == "/hello-world":
+        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+    else:
+        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /hello-world.")
 
 
 bot.polling(none_stop=True, interval=0)
